7413be11426c9acfd39544034e3f9bcf6241b2a3
phong <lephong.xyz@gmail.com>
2017-02-15 16:51:53 +0100

    if n_updates < 0: run until converge (diff < 1e-5)

--- src/meanshift.py
+++ src/meanshift.py
@@ -3,13 +3,13 @@ import numpy as np
 import matplotlib.pyplot as plt
 import time
 
-n_samples = 300 # sample size
+n_samples = 700 # sample size
 n_centroids = n_samples # number of centroids
-n_updates = 50 # number of updates
+n_updates = -1 # 50 # number of updates
 
 n_gaussians = 3 # 'true' number of clusters
 
-window_radius = .1
+window_radius = .05
 dim = 2
 input_X = tf.placeholder(tf.float32, [None, dim])
 init_C = tf.placeholder(tf.float32, [None, dim])
@@ -38,12 +38,13 @@ def create_data(n_points, n_components):
     return points
 
 
-def mean_shift(n_updates):
+def mean_shift(n_updates=-1):
     X1 = tf.expand_dims(tf.transpose(input_X), 0)
     X2 = tf.expand_dims(input_X, 0)
     C = init_C
 
-    sbs_C = [init_C]
+    sbs_C = tf.TensorArray(dtype=tf.float32, size=10000, infer_shape=False)
+    sbs_C = sbs_C.write(0, init_C)
 
     def _mean_shift_step(C):
         C = tf.expand_dims(C, 2)
@@ -54,11 +55,28 @@ def mean_shift(n_updates):
         C = num / denom
         return C
 
-    for i in range(n_updates):
-        C = _mean_shift_step(C)
-        sbs_C.append(C)
+    if n_updates > 0:
+        for i in range(n_updates):
+            C = _mean_shift_step(C)
+            sbs_C = sbs_C.write(i + 1, C)
+    else:
+        def _mean_shift(i, C, sbs_C, max_diff):
+            new_C = _mean_shift_step(C)
+            max_diff = tf.reshape(tf.reduce_max(tf.sqrt(tf.reduce_sum(tf.pow(new_C - C, 2), axis=1))), [])
+            sbs_C = sbs_C.write(i + 1, new_C)
+            return i + 1, new_C, sbs_C, max_diff
 
-    return C, tf.pack(sbs_C)
+        def _cond(i, C, sbs_C, max_diff):
+            return max_diff > 1e-5
+
+        n_updates, C, sbs_C, _ = tf.while_loop(cond=_cond,
+                                       body=_mean_shift,
+                                       loop_vars=(tf.constant(0), C, sbs_C, tf.constant(1e10)))
+
+        n_updates = tf.Print(n_updates, [n_updates])
+
+
+    return C, sbs_C.gather(tf.range(n_updates + 1))
 
 
 def plot(X, C, sbs_C):
@@ -85,7 +103,7 @@ if __name__ == "__main__":
     C, sbs_C = sess.run([ms_C, sbs_ms_C], feed_dict={input_X: stacked_X, init_C: C})
     print(time.process_time() - t)
 
-    sbs_C = np.reshape(sbs_C, [n_updates + 1, C.shape[0], C.shape[1]])
+    sbs_C = np.reshape(sbs_C, [-1, C.shape[0], C.shape[1]])
 
     plot(X, C, sbs_C)
 
